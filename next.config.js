75859240ba87a90b33ba0da7e94a74817b81f422
rajxzdev <rajxz8273@gmail.com>
2026-06-28 23:01:07 +0700

    Upload next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.ytimg.com' },
+      { protocol: 'https', hostname: 'img.youtube.com' },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
